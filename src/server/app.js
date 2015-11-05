05501ca22c360faf9540903ca63f9a1f8165c2c7
Igor Ili <ilicigor@outlook.com>
2015-11-05 22:57:37 +0100

    refactoring and crud users

--- src/server/app.js
+++ src/server/app.js
@@ -1,44 +1,12 @@
-var express = require('express');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var mongoose = require('./config/mongoose');
+var express = require('./config/express');
+
+var db = mongoose();
+
 var app = express();
-var bodyParser = require('body-parser');
-var compress = require('compression');
-var cors = require('cors');
-// var errorHandler = require('./routes/utils/errorHandler')();
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var port = process.env.PORT || 6000;
+app.listen(3000);
+module.exports = app;
 
-var environment = process.env.NODE_ENV;
-
-// app.use(favicon(__dirname + '/favicon.ico'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-app.use(compress());
-app.use(logger('dev'));
-app.use(cors());
-// app.use(errorHandler.init);
-
-
-switch (environment) {
-    case 'build':
-        console.log('** BUILD **');
-        app.use(express.static('./build/'));
-        app.use('/*', express.static('./build/index.html'));
-        break;
-    default:
-        console.log('** DEV **');
-        app.use(express.static('./src/client/'));
-        app.use(express.static('./'));
-        app.use(express.static('./tmp'));
-        app.use('/*', express.static('./src/client/index.html'));
-        break;
-}
-
-app.listen(port, function() {
-    console.log('Express server listening on port ' + port);
-    console.log('env = ' + app.get('env') +
-                '\n__dirname = ' + __dirname +
-                '\nprocess.cwd = ' + process.cwd());
-});
+console.log('Server is up and running at port 3000');
---
+++ src/server/routes/index.server.routes.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+module.exports = function(app) {
+	var index = require('../controllers/index.server.controller');
+	app.get('/', index.render);
+	
+};
